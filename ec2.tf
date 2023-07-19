2130813d25a86636289cf9567bd1050f24cd3376
LJS <js.insideinfo@gmail.com>
2023-07-19 13:14:52 +0900

    subnet add

--- ec2.tf
+++ ec2.tf
@@ -3,9 +3,10 @@ provider "aws" {
 }
 
 resource "aws_instance" "ec2" {
-    ami = "ami-068d6fd750a3617dd"
+    ami = "ami-0abc4c35ba4c005ca"
     instance_type = "t2.micro"
     subnet_id = "subnet-0e3124a38d1724f4c"
+    security_groups = "sg-03af6a452c389eb45"
 }
 
 output "ec2_ip" {
