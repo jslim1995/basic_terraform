aaecf8b6c64cb464fc27cf71d4ca196dcd1f9f7d
LJS <js.insideinfo@gmail.com>
2023-07-19 13:44:32 +0900

    user data test

--- ec2.tf
+++ ec2.tf
@@ -8,10 +8,17 @@ resource "aws_instance" "ec2" {
     subnet_id = "subnet-0e3124a38d1724f4c"
     security_groups = [ "sg-03af6a452c389eb45" ]
     key_name = "jinsu"
+    tagtags = {
+        Name = "Terraform_Test"
+    }
     root_block_device {
         volume_type = "gp3"
         volume_size = "10"
     }
+    user_data = <<EOF
+      #!/bin/bash
+      sudo echo test | tee test.txt
+EOF
 }
 
 output "ec2_ip" {
