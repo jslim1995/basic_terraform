ec5bdd2e653bcdaec976db5f15df3e134decce47
LJS <js.insideinfo@gmail.com>
2023-07-20 16:30:25 +0900

    template_file을 사용하여 user_data 적용 4

--- ec2.tf
+++ ec2.tf
@@ -4,7 +4,7 @@ variable "ec2-count" {
 }
 
 resource "template_file" "user_data" {
-    template = file("shell_script.tpl")
+    template = "${file("shell_script.tpl")}"
 
     vars = {
         dir_name = "${aws_instance.ec2.tags}"
