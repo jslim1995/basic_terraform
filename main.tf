ac45ccce3357a9865c21d9f266600c41ab752e8d
LJS <js.insideinfo@gmail.com>
2023-07-20 15:50:57 +0900

    퍼블릭 IP 자동할당 및 라우터-서브넷 연결

--- main.tf
+++ main.tf
@@ -14,11 +14,15 @@ resource "aws_vpc" "main" {
   
 }
 
+// https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/subnet
 resource "aws_subnet" "sb" {
     count = length(var.subnet_az_list)
     vpc_id = aws_vpc.main.id
+    
     availability_zone = var.subnet_az_list[count.index].availability_zone
     cidr_block = var.subnet_az_list[count.index].cidr_block
+
+    map_public_ip_on_launch = true
 
     tags = {
         Name = "${var.prefix}-subnet-public1-${var.subnet_az_list[count.index].availability_zone}"
@@ -34,6 +38,7 @@ resource "aws_internet_gateway" "gw" {
   
 }
 
+// https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route_table
 resource "aws_route_table" "rtb" {
     vpc_id = aws_vpc.main.id
     route {
@@ -44,6 +49,13 @@ resource "aws_route_table" "rtb" {
     tags = {
         Name = "${var.prefix}-rtb-public"
     }
+}
+
+// https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route_table_association
+resource "aws_route_table_association" "rtb_sn" {
+    count = length(var.subnet_az_list)
+    subnet_id = aws_subnet.sb[count.index].id
+    route_table_id = aws_route_table.rtb.id
 }
 
 
