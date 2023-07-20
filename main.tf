9ae933ab700c530435f2c923fbc084db83fec37a
LJS <js.insideinfo@gmail.com>
2023-07-20 13:03:51 +0900

    route table update

--- main.tf
+++ main.tf
@@ -34,13 +34,13 @@ resource "aws_internet_gateway" "gw" {
 
 resource "aws_route_table" "rtb" {
     vpc_id = aws_vpc.main.id
-    route = {
+    route {
         cidr_block = "0.0.0.0/0"
         gateway_id = aws_internet_gateway.gw.id
     }
 
     tags = {
-        Name = "${var.prefix}-rrb-public"
+        Name = "${var.prefix}-rtb-public"
     }
 }
 
