fc2744164921e285a2e9c24ff878f73d9639edfb
LJS <js.insideinfo@gmail.com>
2023-07-20 11:03:53 +0900

    subnet_az_list update

--- variables.tf
+++ variables.tf
@@ -4,16 +4,13 @@ variable "prefix" {
 }
 
 variable "subnet_az_list" {
-    type = map(object(
-        {
-            availability_zone = "ca-central-1a"
-            cidr_block = "172.164.1.0/24"
-        },
-        {
-            availability_zone = "ca-central-1b"
-            cidr_block = "172.164.2.0/24"
-        }
-    ))
+    type = map(object({
+        availability_zone = "ca-central-1a"
+        cidr_block = "172.164.1.0/24"
+    }), object({
+        availability_zone = "ca-central-1b"
+        cidr_block = "172.164.2.0/24"
+    }))
     description = "az list"
 }
 
