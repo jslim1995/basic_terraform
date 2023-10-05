1a1edb52da09225c11f37fd937144d3b4ede0b60
LJS <js.insideinfo@gmail.com>
2023-10-05 16:38:13 +0900

    test end

--- ec2.tf
+++ ec2.tf
@@ -29,65 +29,64 @@ data "template_file" "user_data" {
     }
 }
 
-# resource "aws_instance" "vault_raft_amz2_x86" {
-
-#     ami = var.ami_amz2_x86
-#     instance_type = "t2.micro"
-#     count = var.ec2-count
-#     subnet_id = aws_subnet.sb[(tonumber(count.index)+1)%length(var.subnet_az_list)].id
-#     security_groups = [ aws_security_group.all.id ]
-#     key_name = "jinsu"
-#     tags = {
-#         Name = "${var.prefix}-Test-${count.index}"
-#         service = "${var.vault_auto_join}"
-#     }
-#     root_block_device {
-#         volume_type = "gp3"
-#         volume_size = "10"
-#         tags = {
-#             Name = "${var.prefix}_Test_Volume_${count.index}"
-#         }
-#     }
-#     credit_specification {
-#         cpu_credits = "standard"
-#     }
-    
-#     iam_instance_profile = aws_iam_instance_profile.vault_join_profile.name
-#     # templatefile function 사용
-#     # user_data = templatefile("user_data.tpl", {
-#     #     # dir_name = "${var.prefix}-Test-${count.index}"
-#     #     INSTANCE_ID = var.INSTANCE_ID2
-#     #     TAG = var.vault_auto_join
-#     #     vault_license = var.VAULT_LICENSE
-#     # })
-    
-#     user_data = data.template_file.user_data.rendered
-    
-#     lifecycle {
-#         ignore_changes = [ user_data ]
-#     }
-# }
-
-
-resource "aws_instance" "test" {
+resource "aws_instance" "vault_raft_amz2_x86" {
     ami = var.ami_amz2_x86
     instance_type = "t2.micro"
-    availability_zone = var.subnet_az_list[0].availability_zone
-    subnet_id = aws_subnet.sb[0].id
-    # security_group = [ aws_security_group.all.id ]
+    count = var.ec2-count
+    subnet_id = aws_subnet.sb[(tonumber(count.index)+1)%length(var.subnet_az_list)].id
+    vpc_security_group_ids = [ aws_security_group.all.id ]
     key_name = "jinsu"
     tags = {
-        Name = "${var.prefix}-Test"
+        Name = "${var.prefix}-Test-${count.index}"
         service = "${var.vault_auto_join}"
     }
-    # root_block_device {
-    #     volume_type = "gp3"
-    #     volume_size = "10"
-    #     tags = {
-    #         Name = "${var.prefix}_Test_Volume"
-    #     }
+    root_block_device {
+        volume_type = "gp3"
+        volume_size = "10"
+        tags = {
+            Name = "${var.prefix}_Test_Volume_${count.index}"
+        }
+    }
+    credit_specification {
+        cpu_credits = "standard"
+    }
+    
+    iam_instance_profile = aws_iam_instance_profile.vault_join_profile.name
+    # templatefile function 사용
+    # user_data = templatefile("user_data.tpl", {
+    #     # dir_name = "${var.prefix}-Test-${count.index}"
+    #     INSTANCE_ID = var.INSTANCE_ID2
+    #     TAG = var.vault_auto_join
+    #     vault_license = var.VAULT_LICENSE
+    # })
+    
+    user_data = data.template_file.user_data.rendered
+    
+    # lifecycle {
+    #     ignore_changes = [ user_data ]
     # }
 }
+
+
+# resource "aws_instance" "test" {
+#     ami = var.ami_amz2_x86
+#     instance_type = "t2.micro"
+#     availability_zone = var.subnet_az_list[0].availability_zone
+#     subnet_id = aws_subnet.sb[0].id
+#     vpc_security_group_ids = [ aws_security_group.all.id ]
+#     key_name = "jinsu"
+#     tags = {
+#         Name = "${var.prefix}-Test"
+#         service = "${var.vault_auto_join}"
+#     }
+#     # root_block_device {
+#     #     volume_type = "gp3"
+#     #     volume_size = "10"
+#     #     tags = {
+#     #         Name = "${var.prefix}_Test_Volume"
+#     #     }
+#     # }
+# }
 
 # test 할 것
 # null_resource : https://registry.terraform.io/providers/hashicorp/null/latest/docs/resources/resource
