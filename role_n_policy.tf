ac2356199801c7aef638028e04217f46c76c1d44
LJS <js.insideinfo@gmail.com>
2023-10-05 10:57:23 +0900

    role mod

--- role_n_policy.tf
+++ role_n_policy.tf
@@ -1,6 +1,5 @@
 # 참고 : https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role_policy
 data "aws_iam_policy_document" "instance_assume_role_policy" {
-    Version = "2012-10-17"
     Statement {
         Effect = "Allow"
         principals {
