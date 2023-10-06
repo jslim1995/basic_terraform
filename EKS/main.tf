c954f963c01cc5fb421520c1534fe1d4a294d504
LJS <js.insideinfo@gmail.com>
2023-10-06 10:21:29 +0900

    node version 1.24

--- EKS/main.tf
+++ EKS/main.tf
@@ -28,18 +28,26 @@ output "endpoint" {
 # EKS Node Group
 resource "aws_eks_node_group" "eks_node" {
     cluster_name    = aws_eks_cluster.eks_cluster.name
-    node_group_name = "${var.prefix}_eks_node"
     node_role_arn   = aws_iam_role.eks_worker_node_role.arn
     subnet_ids      = var.subnet_ids
-
-    instance_types = [ "t3.small" ]
     scaling_config {
         desired_size = 1
         max_size     = 2
         min_size     = 1
     }
 
+    # Option
+    node_group_name = "${var.prefix}_eks_node"
+    instance_types = [ "t3.small" ]
+
+    # remote_access {
+    #     ec2_ssh_key = "jinsu"
+    #     source_security_group_ids = var.security_group_ids
+    # }
+
     update_config {
         max_unavailable = 1
     }
+
+    version = "1.24"
 }
