10d4cc086dbad4d2b31647cc8c818f9bf6ff5a6a
LJS <js.insideinfo@gmail.com>
2023-11-14 11:02:58 +0900

    kv write 2

--- vault/main.tf
+++ vault/main.tf
@@ -1,19 +1,19 @@
 provider "vault" {
-#   address    = "http://jinsu.inside-vault.com:80"
+  #   address    = "http://jinsu.inside-vault.com:80"
   address    = "http://3.96.222.156:8200"
   token      = "hvs.CAESICdREjsQL3w3ueiAEA4wvUYT3wE07jnvpWxbsUyF70nOGigKImh2cy5nSm9YVjluM3R3YmJKY3pLS2FGNkFkaUQuUWJMckMQ36AB"
   token_name = "terraform"
-#   auth_login {
-#     path      = "auth/userpass/login/:username"
-#     namespace = "tf"
-#     # use_root_namespace = true
-#   }
+  #   auth_login {
+  #     path      = "auth/userpass/login/:username"
+  #     namespace = "tf"
+  #     # use_root_namespace = true
+  #   }
 }
 
 # kv write
 # https://registry.terraform.io/providers/hashicorp/vault/latest/docs/resources/kv_secret_v2
 resource "vault_mount" "kvv2" {
-  namespace = "tf"
+  namespace   = "tf"
   path        = "secret"
   type        = "kv"
   options     = { version = "2" }
@@ -21,10 +21,11 @@ resource "vault_mount" "kvv2" {
 }
 
 resource "vault_kv_secret_v2" "example" {
-  mount               = vault_mount.kvv2.path
-  name                = "value"
-#   cas                 = 1
-#   delete_all_versions = true
+  namespace = "tf"
+  mount     = vault_mount.kvv2.path
+  name      = "value"
+  #   cas                 = 1
+  #   delete_all_versions = true
   data_json = jsonencode(
     {
       zip = "zap",
@@ -39,5 +40,5 @@ data "vault_kv_secret_v2" "prefix" {
   namespace = "tf"
   mount     = "secret"
   name      = "value"
-#   version   = "2"
+  #   version   = "2"
 }
