917fe935ea02ec2407c3d22efb7c767f320568f8
LJS <js.insideinfo@gmail.com>
2023-11-14 14:02:40 +0900

    data 뽑아써볼까나?

--- vault/output.tf
+++ vault/output.tf
@@ -1,4 +1,4 @@
 output "prefix_vault" {
-  value       = data.vault_kv_secret_v2.prefix.data_json
+  value       = data.vault_kv_secret_v2.prefix.data_json[0]
   description = "servername prefix"
 }
