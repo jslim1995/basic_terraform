efbbe955c409e0a58eb6ea9705446c540672095e
LJS <js.insideinfo@gmail.com>
2023-11-14 13:35:49 +0900

    ㅇㅇ

--- vault/output.tf
+++ vault/output.tf
@@ -1,4 +1,4 @@
-output "prefix_vault" {
-  value       = data.vault_kv_secret_v2.prefix
-  description = "servername prefix"
-}
+# output "prefix_vault" {
+#   value       = data.vault_kv_secret_v2.prefix
+#   description = "servername prefix"
+# }
