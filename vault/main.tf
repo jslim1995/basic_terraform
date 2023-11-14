35647c95ac85ded9930a4abf0c64c533c6e08205
LJS <js.insideinfo@gmail.com>
2023-11-14 09:34:16 +0900

    vault ip

--- vault/main.tf
+++ vault/main.tf
@@ -1,5 +1,6 @@
 provider "vault" {
-  address    = "http://jinsu.inside-vault.com:80"
+#   address    = "http://jinsu.inside-vault.com:80"
+  address    = "http://3.96.222.156:8200"
   token      = "hvs.CAESIBDiNwQkE9npx-bz5v1aYDMKi7LfxNAuAjbRMG3Tu4GzGigKImh2cy5KU3ZPRFNXR1NLVUdsVXU5TVJIYmRsNFcuUWJMckMQyKAB"
   token_name = "terraform"
   auth_login {
