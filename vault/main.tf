857f34e8843b0f20739ae35f2eb0209a4dd1715c
LJS <js.insideinfo@gmail.com>
2023-11-14 14:42:43 +0900

    token 수정

--- vault/main.tf
+++ vault/main.tf
@@ -2,7 +2,7 @@
 provider "vault" {
   #   address    = "http://jinsu.inside-vault.com:80"
   address    = "http://3.96.222.156:8200"
-  token      = "hvs.CAESICdREjsQL3w3ueiAEA4wvUYT3wE07jnvpWxbsUyF70nOGigKImh2cy5nSm9YVjluM3R3YmJKY3pLS2FGNkFkaUQuUWJMckMQ36AB"
+  token      = "hvs.CAESICF2PJhCahhLNgy8cC9kEeh4GnX7ESwgiR0ABtBUDTdkGigKImh2cy5HRUQ3YTQzM2VpdGZtNmd1WE9xeHBrZGwuUWJMckMQ2KMB"
   token_name = "terraform"
   #   auth_login {
   #     path      = "auth/userpass/login/:username"
