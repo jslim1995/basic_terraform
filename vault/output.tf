0963ea36ffb845fe472572fb26db5041bce79506
LJS <js.insideinfo@gmail.com>
2023-11-14 11:14:06 +0900

    read 가보자고~?

--- vault/output.tf
+++ vault/output.tf
@@ -1,4 +1,4 @@
-# output "prefix" {
-#   value       = data.vault_kv_secret_v2.prefix
-#   description = "servername prefix"
-# }
+output "prefix" {
+  value       = data.vault_kv_secret_v2.prefix
+  description = "servername prefix"
+}
