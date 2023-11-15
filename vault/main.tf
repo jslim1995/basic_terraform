b1a3e8853fc67ed337177fa56e066657d5592b38
LJS <js.insideinfo@gmail.com>
2023-11-15 16:22:57 +0900

    vault

--- vault/main.tf
+++ vault/main.tf
@@ -1,46 +1,46 @@
-# # https://registry.terraform.io/providers/hashicorp/vault/latest/docs
-# provider "vault" {
-#   address = "http://jinsu.inside-vault.com"
-#   # address    = "http://3.96.222.156:8200"
-#   token      = "hvs.CAESICF2PJhCahhLNgy8cC9kEeh4GnX7ESwgiR0ABtBUDTdkGigKImh2cy5HRUQ3YTQzM2VpdGZtNmd1WE9xeHBrZGwuUWJMckMQ2KMB"
-#   token_name = "terraform"
-#   #   auth_login {
-#   #     path      = "auth/userpass/login/:username"
-#   #     namespace = "tf"
-#   #     # use_root_namespace = true
-#   #   }
+# https://registry.terraform.io/providers/hashicorp/vault/latest/docs
+provider "vault" {
+  address = "http://jinsu.inside-vault.com"
+  # address    = "http://3.96.222.156:8200"
+  token      = "hvs.CAESIJlROI1KjOmr2-Cnp4DhC5EsAq0FJ39XNIHH3kOhl77iGigKImh2cy5oZzRXQ0pPbjZ3aGJ0MzJkUFBMSElNZEQuUWJMckMQmaYB"
+  token_name = "terraform"
+  #   auth_login {
+  #     path      = "auth/userpass/login/:username"
+  #     namespace = "tf"
+  #     # use_root_namespace = true
+  #   }
+}
+
+# kv enable
+# https://registry.terraform.io/providers/hashicorp/vault/latest/docs/resources/kv_secret_v2
+resource "vault_mount" "kvv2" {
+  #   namespace   = "tf"
+  path        = "client_count_test"
+  type        = "kv"
+  options     = { version = "2" }
+  description = "KV Version 2 secret engine mount"
+}
+
+# kv write
+# resource "vault_kv_secret_v2" "example" {
+# #   namespace = "tf"
+#   mount     = "secret"
+#   name      = "value"
+#   #   cas                 = 1
+#   #   delete_all_versions = true
+#   data_json = jsonencode(
+#     {
+#       zip = "zap2",
+#       foo = "bar2"
+#     }
+#   )
 # }
 
-# # kv enable
-# # https://registry.terraform.io/providers/hashicorp/vault/latest/docs/resources/kv_secret_v2
-# # resource "vault_mount" "kvv2" {
-# #   #   namespace   = "tf"
-# #   path        = "client_count_test"
-# #   type        = "kv"
-# #   options     = { version = "2" }
-# #   description = "KV Version 2 secret engine mount"
-# # }
-
-# # kv write
-# # resource "vault_kv_secret_v2" "example" {
-# # #   namespace = "tf"
-# #   mount     = "secret"
-# #   name      = "value"
-# #   #   cas                 = 1
-# #   #   delete_all_versions = true
-# #   data_json = jsonencode(
-# #     {
-# #       zip = "zap2",
-# #       foo = "bar2"
-# #     }
-# #   )
-# # }
-
-# # kv read
-# # https://registry.terraform.io/providers/hashicorp/vault/latest/docs/data-sources/kv_secret_v2
-# data "vault_kv_secret_v2" "prefix" {
-#   #   namespace = "tf"
-#   mount = "secret"
-#   name  = "value"
-#   #   version   = "2"
-# }
+# kv read
+# https://registry.terraform.io/providers/hashicorp/vault/latest/docs/data-sources/kv_secret_v2
+data "vault_kv_secret_v2" "prefix" {
+  #   namespace = "tf"
+  mount = "secret"
+  name  = "value"
+  #   version   = "2"
+}
