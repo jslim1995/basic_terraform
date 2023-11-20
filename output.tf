04bb3e0f249bca93e4ebaaf09d251e308d722aa3
LJS <js.insideinfo@gmail.com>
2023-11-20 10:55:32 +0900

    remote-exec test

--- output.tf
+++ output.tf
@@ -1,26 +1,26 @@
-output "vpc_id" {
-  value       = aws_vpc.main.id
-  description = "VPC id"
-}
+# output "vpc_id" {
+#   value       = aws_vpc.main.id
+#   description = "VPC id"
+# }
 
 # output "ec2_ip" {
 #     value = aws_instance.vault_raft_amz2_x86.*.private_ip
 #     description = "PrivateIP address details"
 # }
 
-output "subnet_ids" {
-  value = aws_subnet.sb.*.id
-}
+# output "subnet_ids" {
+#   value = aws_subnet.sb.*.id
+# }
 
-output "VAULT_LICENSE" {
-  value     = var.VAULT_LICENSE
-  sensitive = true
-}
+# output "VAULT_LICENSE" {
+#   value     = var.VAULT_LICENSE
+#   sensitive = true
+# }
 
-output "prefix" {
-  value       = var.prefix
-  description = "servername prefix"
-}
+# output "prefix" {
+#   value       = var.prefix
+#   description = "servername prefix"
+# }
 
 output "pemfile" {
   value = data.tls_public_key.pemfile.private_key_pem
